9003e1b7d1908e03d7efe29042800cc3b6f6d366
Hackbright Student <no-reply@hackbrightacademy.com>
2019-10-29 17:58:38 -0700

    added routes for register, login, logout

--- server.py
+++ server.py
@@ -2,10 +2,11 @@
 
 from jinja2 import StrictUndefined
 
-from flask import Flask
+from flask import (Flask, render_template, redirect, request, flash,
+                   session)
 from flask_debugtoolbar import DebugToolbarExtension
 
-from model import connect_to_db, db
+from model import User, Rating, Movie, connect_to_db, db
 
 
 app = Flask(__name__)
@@ -22,7 +23,69 @@ app.jinja_env.undefined = StrictUndefined
 @app.route('/')
 def index():
     """Homepage."""
-    return "<html><body>Placeholder for the homepage.</body></html>"
+    return render_template("homepage.html")
+
+
+@app.route("/users")
+def user_list():
+    """Show list of users."""
+
+    users = User.query.all()
+    return render_template("user_list.html", users=users)
+
+@app.route("/register", methods=["GET"])
+def register_form():
+
+    return render_template("register_form.html")
+
+
+@app.route("/register", methods=["POST"])
+def register_process():
+
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    new_user = User(email=email, password=password)
+
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect("/")
+
+
+@app.route("/login", methods=["GET"])
+def login_form():
+
+    return render_template("login_form.html")
+
+
+@app.route("/login", methods=["POST"])
+def login_process():
+
+    
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    existing_email = User.query.filter_by(email=email).one()
+    if email == existing_email.email:
+        if password == existing_email.password:
+            session['user_id'] = existing_email.user_id
+            session['email'] = existing_email.email
+            flash('You were successfully logged in')
+            return redirect("/")
+    else:
+        flash('Invalid login or password')
+
+    
+    return redirect("/login")
+
+
+@app.route("/logout")
+def logout():
+    
+    session.clear()
+    flash('You were successfully logged out!')
+    return redirect("/")
+
 
 
 if __name__ == "__main__":
